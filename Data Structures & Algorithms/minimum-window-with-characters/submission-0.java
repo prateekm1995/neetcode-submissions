2acff2dcf07fa3323deb0d472d8f61bcaed2bc00
Prateek Malhotra <prateekm1995@gmail.com>
2026-07-14 09:48:43 +0530

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.java
@@ -0,0 +1,75 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        String result = new String();
+        int l=0,r=0;
+        //target map
+        Map<Character, Integer> target = new HashMap();
+        Map<Character, Integer> tracker = new HashMap();
+
+        for(char c: t.toCharArray()) {
+            target.put(c, target.getOrDefault(c, 0) + 1);
+            tracker.put(c, 0);
+        }
+
+        boolean isWindowOpen=false;
+
+        if(t.length() > s.length()) {
+            return result;
+        }
+
+        while(r<s.length()) {
+            if(isWindowOpen){
+                if(isWindowComplete(target, tracker)){
+                    if(result.isEmpty() || result.length() > (r-l)) {
+                        result = s.substring(l, r);
+                    }
+                    tracker.put(s.charAt(l), tracker.get(s.charAt(l)) - 1);
+                    l++;
+                    while(l < r && !target.keySet().contains(s.charAt(l))) {
+                        l++;
+                    }
+                } else {
+                    if(tracker.keySet().contains(s.charAt(r))) {
+                        tracker.put(s.charAt(r), tracker.get(s.charAt(r)) + 1);
+                    }
+                    r++;
+                }
+                if(l == r) {
+                    isWindowOpen = false;
+                }
+            } else {
+                if(tracker.containsKey(s.charAt(l))) {
+                    tracker.put(s.charAt(r), tracker.get(s.charAt(r)) + 1);
+                    r++;
+                } else {
+                    l++;
+                    r++;
+                }
+                if(l != r) {
+                    isWindowOpen = true;
+                }
+            }
+        }
+        while(isWindowComplete(target, tracker)){
+            if(result.isEmpty() || result.length() > (r-l)) {
+                result = s.substring(l, r);
+            }
+            tracker.put(s.charAt(l), tracker.get(s.charAt(l)) - 1);
+            l++;
+            while(l < r && !target.keySet().contains(s.charAt(l))) {
+                l++;
+            }
+        }
+
+        return result;
+    }
+
+    private boolean isWindowComplete(Map<Character, Integer> target, Map<Character, Integer> tracker) {
+        for(Character key : target.keySet()) {
+            if(tracker.get(key) < target.get(key)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
